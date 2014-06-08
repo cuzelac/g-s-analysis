04c4a9376fc432c555612fdc7c241e750a38da6f
Chris Uzelac <cuzelac@gmail.com>
2014-06-08 09:27:12 -0700

    working stacked bar chart

--- load_canon.rb
+++ load_canon.rb
@@ -9,16 +9,25 @@ require 'json'
 # series: [{name: 'type 1', data: [s1t1, s2t1]},
 #          {name: 'type 2', data: [s1t2, s2t2]}]
 
-def build_stacked_bar(canon)
-  shows = canon.collect {|show| show[:name]}
+def build_stacked_bar(canon, restrict_types=true)
+  shows = canon.collect {|show| show[:name]} #.sort_by {|n| PUBLICATION_ORDER.find_index(n)}
 
   # we'll store
-  # h[type] = [
+  # h[type] = [num_types_in_show1, num_types_in_show2 ...]
   collection = {}
+
+  keep_types = [:song, :dialog, :recit, :finale]
 
   # initialize the collection hash
   canon.each do |show|
     show[:scenes].each {|scene| collection[scene[:type]] = []}
+  end
+
+  # keep only the types we want to keep
+  if restrict_types
+    collection.delete_if do |type, v|
+      !keep_types.include?(type.to_sym)
+    end
   end
 
   canon.each do |show|
@@ -27,11 +36,20 @@ def build_stacked_bar(canon)
     collection.keys.each do |type|
       count = 0
       scenes.each do |scene|
-        # IMPLEMENT ME
+        count += 1 if scene[:type] == type
       end
       collection[type] << count
     end
   end
+
+  series = []
+  collection.each do |type,arr|
+    series << {"name" => type,
+     "data" => arr
+    }
+  end
+
+  return shows, series
 end
 
 # scene_record
@@ -44,7 +62,9 @@ end
 #   scenes: [scene_record1, scene_record2]
 
 CANON = YAML.load(File.read("./g-s-breakdown.yaml"))
+PUBLICATION_ORDER = ["trial-by-jury", "sorcerer", "pinafore", "pirates", "patience", "iolanthe", "princess-ida", "mikado", "ruddigore", "yeoman", "gondoliers", "utopia-ltd", "grand-duke"]
 
+categories, series = build_stacked_bar(CANON)
 binding.pry
 
 # vim: ft=ruby
